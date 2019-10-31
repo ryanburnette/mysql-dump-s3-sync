75aeba741cd4b120cafbdff05f1bd2fd6b9eb347
Ryan Burnette <ryan.burnette@gmail.com>
2019-10-31 12:24:32 -0400

    progress

--- index.js
+++ index.js
@@ -1,87 +1,142 @@
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-if (fs.existsSync('.env')) {
-  require('dotenv').config({});
-}
 var promisify = require('util').promisify;
 var mkdirp = promisify(require('@root/mkdirp'));
 var exec = promisify(require('child_process').exec);
 var aws = require('aws-sdk');
-var s3 = new aws.S3({signatureVersion: 'v4'});
+var fs = require('fs');
 var readFile = promisify(fs.readFile);
-var cron = require('node-cron');
+var formatDate = require('./lib/format-date');
+var parseMysqlUrl = require('@ryanburnette/parse-mysql-url');
 
-var db_host = process.env.DB_HOST || 'localhost';
-var db_port;
-var db_user = process.env.DB_USER;
-var db_pass = process.env.DB_PASS;
-var db_name = process.env.DB_NAME;
+function backup(opts) {
+  if (!opts) {
+    opts = {};
+  }
 
-if (db_host.includes(':')) {
-  var parts = db_host.split(':');
-  db_host = parts[0];
-  db_port = parts[1];
-}
+  var s3 = new aws.S3({ signatureVersion: 'v4' });
 
-function backup() {
-  var backupDir = formatDate(new Date());
-  mkdirp(backupDir)
+  if (!opts.db) {
+    opts.db = {};
+  }
+
+  if (process.env.DB_URL) {
+    opts.db = parseMysqlUrl(process.env.DB_URL);
+  }
+
+  if (process.env.DB_HOST) {
+    opts.db.host = process.env.DB_HOST;
+  }
+
+  if (!opts.db.host) {
+    opts.db.host = 'localhost';
+  }
+
+  if (opts.db.host.includes(':')) {
+    var parts = opts.db.host.split(':');
+    opts.db.host = parts[0];
+    opts.db.port = parts[1];
+  }
+
+  if (process.env.DB_PORT) {
+    opts.db.port = process.env.DB_PORT;
+  }
+
+  if (!opts.db.port) {
+    opts.db.port = 3306;
+  }
+
+  if (process.env.DB_USERNAME) {
+    opts.db.username = process.env.DB_USERNAME;
+  }
+
+  if (!opts.db.username) {
+    throw new Error('opts.db.username is required');
+  }
+
+  if (process.env.DB_PASSWORD) {
+    opts.db.password = process.env.DB_PASSWORD;
+  }
+
+  if (!opts.db.password) {
+    throw new Error('opts.db.password is required');
+  }
+
+  if (process.env.DB_NAME) {
+    opts.db.name = process.env.DB_NAME;
+  }
+
+  if (!opts.db.name) {
+    throw new Error('opts.db.name is required');
+  }
+
+  if (process.env.LOGGING == 'true') {
+    opts.logging = true;
+  }
+
+  var tmpdir = formatDate();
+
+  function _log(msg) {
+    if (opts.logging == 'true') {
+      console.log(msg);
+    }
+  }
+
+  return mkdirp(tmpdir)
     .then(function() {
-      _log(`BACKUP DIR ${backupDir}/ CREATED`);
+      _log(`BACKUP DIR ${tmpdir}/ CREATED`);
     })
     .then(function() {
-      var cmd = `mysqldump -u ${db_user} -p'${db_pass}'`;
-      if (db_host != 'localhost') {
-        cmd = `${cmd} -h ${db_host}`;
+      var cmd = `mysqldump -u ${opts.db.username} -p'${opts.db.password}'`;
+      if (opts.db.host != 'localhost') {
+        cmd = `${cmd} -h ${opts.db.host}`;
       }
-      if (db_port) {
-        cmd = `${cmd} -P ${db_port}`;
+      if (opts.db.port) {
+        cmd = `${cmd} -P ${opts.db.port}`;
       }
-      cmd = `${cmd} ${db_name} > ${backupDir}/${db_name}.sql`;
+      cmd = `${cmd} ${opts.db.name} > ${tmpdir}/${opts.db.name}.sql`;
       return exec(cmd).then(function() {
         _log('DUMPED DB');
       });
     })
     .then(function() {
-      var cmd = `tar czvf ${backupDir}/${db_name}.sql.tar.gz ${backupDir}/${db_name}.sql`;
+      var cmd = `tar czvf ${tmpdir}/${opts.db.name}.sql.tar.gz ${tmpdir}/${opts.db.name}.sql`;
       return exec(cmd).then(function() {
         _log('TAR GZ CREATED');
       });
     })
     .then(function() {
-      var cmd = `rm ${backupDir}/${db_name}.sql`;
+      var cmd = `rm ${tmpdir}/${opts.db.name}.sql`;
       return exec(cmd).then(function() {
         _log('DUMP DELETED');
       });
     })
     .then(function() {
-      return readFile(`${backupDir}/${db_name}.sql.tar.gz`).then(function(
+      return readFile(`${tmpdir}/${opts.db.name}.sql.tar.gz`).then(function(
         buffer
       ) {
         return s3
           .putObject({
-            Bucket: process.env.AWS_BUCKET,
-            Key: `${backupDir}/${db_name}.sql.tar.gz`,
+            Bucket: opts.aws.bucket,
+            Key: `${tmpdir}/${opts.db.name}.sql.tar.gz`,
             Body: buffer
           })
           .promise()
           .then(function() {
             _log(
-              `S3 PUT OBJECT ${db_name}.sql.tar.gz IN BUCKET ${process.env.AWS_BUCKET}`
+              `S3 PUT OBJECT ${opts.db.name}.sql.tar.gz IN BUCKET ${opts.aws.bucket}`
             );
           });
       });
     })
     .then(function() {
-      var cmd = `rm ${backupDir}/${db_name}.sql.tar.gz`;
+      var cmd = `rm ${tmpdir}/${opts.db.name}.sql.tar.gz`;
       return exec(cmd).then(function() {
         _log('TAR GZ DELETED');
       });
     })
     .then(function() {
-      var cmd = `rm -rf ${backupDir}`;
+      var cmd = `rm -rf ${tmpdir}`;
       return exec(cmd).then(function() {
         _log('BACKUP DIR DELETED');
       });
@@ -91,16 +146,4 @@ function backup() {
     });
 }
 
-function _log(msg) {
-  if (process.env.LOGGING) {
-    console.log(msg);
-  }
-}
-
-function formatDate(date) {
-  return require('date-fns/format')(date, 'YYYY-MM-DD');
-}
-
-cron.schedule(process.env.CRON_SCHEDULE || '0 0 * * *', function() {
-  backup();
-});
+module.exports = backup;
